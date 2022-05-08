cebfc36c5ba5c55b73514e146c5b020d37783ce5
Karen Costa <karen.pcosta@gmail.com>
2022-05-08 03:28:39 -0300

    feat: add display on HTML function to buttons

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-let chosenServices = []
+let chosenService = []
+
+
 const washBtn = document.getElementById("wash-btn")
 const mowBtn = document.getElementById("mow-btn")
 const pullWeeds = document.getElementById("pull-weed-btn")
@@ -6,26 +8,43 @@ const serviceDisplay = document.getElementById("service-display")
 
 
 washBtn.addEventListener("click", function(){
-    const click1 = {
-        washCar: 10,
+    const serviceClicked = { 
+        service: "Wash Car", value: 10
     }
-    chosenServices.push(click1)
+    chosenService.push(serviceClicked)
     washBtn.disabled = true;
-})
+    display()
+}) 
+
 
 mowBtn.addEventListener("click", function(){
-    const click2 = {
-        mowLawn: 20,
-    }
-    chosenServices.push(click2)
+    const serviceClicked = { 
+        service: "Mow Lawn", value: 20
+} 
+    chosenService.push(serviceClicked)
     mowBtn.disabled = true;
+    display()
 })
 
 pullWeeds.addEventListener("click", function(){
-    const click3 = {
-        pullWeeds: 30,
-    }
-    chosenServices.push(click3)
+    const serviceClicked = { 
+        service: "Pull Weeds", value: 30
+} 
+    chosenService.push(serviceClicked)
     pullWeeds.disabled = true;
+    display()
 })
 
+function display() {
+    let display = ""
+    for (let i = 0; i < chosenService.length; i++) {
+        display += `
+        <div class="service-container">
+            <div class="service-name">${chosenService[i].service}</div>
+            <div class="service-value"><span>$</span>${chosenService[i].value}</div>
+        </div>
+       `
+    }
+    serviceDisplay.innerHTML = display
+
+}
